c14d4a48dcac30066e9d9d7ab5ca2cc55736575c
GUY KAMNOEDRAT <guy-k@TYG.NET>
2020-08-13 17:05:58 +0700

    button add

--- src/component/Member.js
+++ src/component/Member.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import TA from './TA'
+import Ta from './Ta'
+import { Button,Box } from '@material-ui/core'
+import SaveIcon from '@material-ui/icons/Save'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    boxhead: {
+      flexGrow: 1,
+    }
+  }));
+  
+
 export default function Member(props) {
+    const classes = useStyles();
     return (
         <div>
-            {props.mname}
-            <TA uid = {props.uid} />
+            <Box className={classes.boxhead}>{props.mname}  
+            <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                startIcon={<SaveIcon />}>Save</Button></Box>
+            <Ta uid = {props.uid} />
         </div>
     )
 }
--- src/component/TA.js
+++ src/component/TA.js
@@ -4,68 +4,46 @@ import Checkbox from '@material-ui/core/Checkbox';
 //import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel'; 
 
-export default function TA() {
-
-    const [talist,setTalist] = React.useState({isCh:false,isCr:false,isRv:true})
-    //let TAs = [{isCh:1,isCr:1,isRv:0}] 
+export default function Ta() {
+    const [talist,setTalist] = React.useState({})
     const handleInputChange=(e)=>{
-        let talist1 = talist
-        talist1[e.target.name]=e.target.checked
-        console.log(talist1)
-        setTalist(talist1)
-        console.log(talist1.isCh)
-    //const handleInputChange = () => 
+        console.log("before")
+        console.log(talist)
+        talist[e.target.name]=e.target.checked
+
+        setTalist(talist)
+        //setTalist({...talist,[e.target.name]:e.target.checked })
+        console.log("after")
+        console.log(talist)
+
     }
     return (
         <div>
         
-        <FormControlLabel
-          control={<Checkbox color="primary" name="isCh"
-          type="checkbox"
-          checked={talist.isCh}
-          onChange={handleInputChange}/>}
-          label="คจ"
-          labelPlacement="top"
-        />
-        <FormControlLabel
-          control={<Checkbox color="primary" name="isCr"
-          type="checkbox"
-          checked={talist.isCr}
-          onChange={handleInputChange}/>}
-          label="คร"
-          labelPlacement="top"
-        />
-        <FormControlLabel
-          control={<Checkbox color="primary" name="isRv"
-          type="checkbox"
-          checked={talist.isRv}
-          onChange={handleInputChange}/>}
-          label="รว"
-          labelPlacement="top"
-        />
-   
-        <div>
-        <input
-            name="isCh"
+          <FormControlLabel
+            control={<Checkbox color="primary" name="isCh"
             type="checkbox"
             checked={talist.isCh}
-            onChange={handleInputChange} />
-        </div>
-  {/*
-        <div>
-            <input
-            name="isCr"
+            onChange={handleInputChange}/>}
+            label="คจ"
+            labelPlacement="top"
+          />
+          <FormControlLabel
+            control={<Checkbox color="primary" name="isCr"
             type="checkbox"
-            checked={TA.isCr}
-            onChange={handleInputChange} />
-        </div>
-        <div>
-            <input
-            name="isRv"
+            checked={talist.isCr}
+            onChange={handleInputChange}/>}
+            label="คร"
+            labelPlacement="top"
+          />
+          <FormControlLabel
+            control={<Checkbox color="primary" name="isRv"
             type="checkbox"
-            checked={TA.isRv}
-            onChange={handleInputChange} />
-        </div> */}
+            checked={talist.isRv}
+            onChange={handleInputChange}/>}
+            label="รว"
+            labelPlacement="top"
+          />
         </div>
     )
 }
